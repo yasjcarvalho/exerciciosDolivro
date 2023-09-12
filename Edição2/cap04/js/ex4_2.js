f459599d252e568a6a95cce7e5b90fb56a222a23
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-09-12 12:58:52 -0300

    intro ao let

--- Edição2/cap04/js/ex4_2.js
+++ Edição2/cap04/js/ex4_2.js
@@ -3,3 +3,21 @@ const frm = document.querySelector('form')
 const resp = document.querySelector('h3')
 
 //"ouvinte" do evento, acionado quando o botão submit for clicado
+
+frm.addEventListener('submit', e => {
+  e.preventDefault()
+  const nome = frm.inNome.value // obtém valores do form
+  const masculino = frm.inMasculino.checked
+  const altura = Number(frm.inAltura.value)
+
+  let peso //declara a variável peso
+  if (masculino) {
+    //(ou, if masculino == true)}
+    peso = 22 * Math.pow(altura, 2) //Math.pow eleva ao quadrado
+  } else {
+    peso = 21 * Math.pow(altura, 2)
+  }
+
+  //apresenta a resposta (altera o conteúdo do elemento h3 da página)
+  resp.innerText = `${nome}: Seu peso ideal é: ${peso.toFixed(3)} kg`
+})
