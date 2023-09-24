64b70b7fd1753249e07c8239224a1fc2f57d5df3
Yasmin Jandriele <97072307+yasjcarvalho@users.noreply.github.com>
2023-09-24 02:43:05 -0300

    ..

--- Edição2/cap09/js/ex9_2.js
+++ Edição2/cap09/js/ex9_2.js
@@ -1,49 +1,53 @@
-const frm = document.querySelector('form')
+const inRadios = document.querySelectorAll('input') // captura tags input da página
 const imClube = document.querySelector('#imgClube')
 const dvTitulo = document.querySelector('#divTitulo')
 
 const trocarClube = () => {
-  let clube //variável que irá receber o nome do clube
+  const clubes = ['Brasil', 'Pelotas', 'Farroupilha'] //vetor com a lista de clubes
 
-  if (frm.rbBrasil.checked) {
-    //verifica qual radiobutton está selecionado
-    clube = 'Brasil'
-  } else if (frm.rbPelotas.checked) {
-    clube = 'Pelotas'
-  } else {
-    clube = 'Farroupilha'
+  let selecao
+  //percorre os inRadios para verificar qual está selecionado
+  for (let i = 0; i < inRadios.length; i++) {
+    if (inRadios[i].checked) {
+      selecao = i //se selecionado, armazena índice do radio selecionado
+      break //e sai da repetição
+    }
   }
-  //define as classes de dvTitulo: row e cores do clube
-  dvTitulo.className = `row cores-${clube}`
-
-  //modifica a imagem de acordo com a seleção do cliente
-  imClube.src = `img/${clube.toLowerCase()}.png`
-  imClube.className = 'img-fluid' //muda o estilo para exibir a imagem
-  imClube.alt = `Símbolo do ${clube}` //modifica o atributo alt
-
-  localStorage.setItem('clube', clube) //salva no navegador a escolha do cliente
+  if (selecao <= 2) {
+    //se a selecao <= 2, então torce por algum clube
+    dvTitulo.className = `row cores-${clubes[selecao]}` //modifica a cor
+    //muda a propriedade src com a imagem do clube selecionado
+    imClube.src = `img/${clubes[selecao].toLowerCase()}.png`
+    imClube.className = 'img-fluid' // muda estilo para exibir imagem
+    imClube.alt = `Símbolo do ${clubes[selecao]}` //texto alternativo
+    localStorage.setItem('clube', clubes[selecao]) //salva nome do clube
+  } else {
+    //else(selecionou "Nenhum")
+    dvTitulo.className = 'row' //tira a classe de cor de divTitulo
+    imClube.className = 'd-none' //oculta imagem
+    imClube.alt = '' //limpa texto alternativo
+    localStorage.removeItem('clube') //remove variável do localStorage
+  }
 }
 
-const inRadios = document.querySelectorAll
-//associa ao evento change de cada botão do form a função trocarClube
-frm.rbBrasil.addEventListener('change', trocarClube)
-frm.rbPelotas.addEventListener('change', trocarClube)
-frm.rbFarroupilha.addEventListener('change', trocarClube)
+//percorre os elementos  para associar function ao evento change
+for (const inRadio of inRadios) {
+  inRadio.addEventListener('change', trocarClube)
+}
 
 const verificarClube = () => {
   if (localStorage.getItem('clube')) {
     //se já estiver salvo algum clube
     const clube = localStorage.getItem('clube') //obtém o nome do clube
-
+    //conforme o clube, marca um dos elementos do input type radio
     if (clube == 'Brasil') {
-      //conforme o clube, marca checked
-      frm.rbBrasil.checked = true
+      inRadios[0].checked = true
     } else if (clube == 'Pelotas') {
-      frm.rbPelotas.checked = true
+      inRadios[1].checked = true
     } else {
-      frm.rbFarroupilha.checked = true
+      inRadios[2].checked = true
     }
-    trocarClube() //chama function que troca imagem e cores
+    trocarClube() //chama a function que troca imagem e cores
   }
 }
 // ao carregar a página, verifica se cliente já selecionou clube anteriormente
